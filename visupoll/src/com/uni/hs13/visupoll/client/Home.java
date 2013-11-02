32e7ee3e938bd9da74a1dad369a90301142e5866
sebastian.stephan <sstephan1337@gmail.com>
2013-11-02 17:19:13 +0100

    Initial Commit. Empty runnable GWT Project.

---
+++ visupoll/src/com/uni/hs13/visupoll/client/Home.java
@@ -0,0 +1,14 @@
+package com.uni.hs13.visupoll.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class Home implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		RootPanel.get().add(new Label("This label was added dymanically by JS ;-)"));
+	}
+
+}
